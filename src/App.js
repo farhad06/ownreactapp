13d5f14a3fe060e2d95876214623f737bdcbabf1
Farhad Ahamed <farhadahamed06@gmail.com>
2021-11-08 23:11:09 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-function App() {
-  return (
-   <div>Hello React</div>
-  );
-}
 
-export default App;
+
+// function App() {
+//   return <div>Hello React</div>;
+// }
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
+// import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-
-
-
-ReactDOM.render(
-    <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+const name = "Farhad Ahamed";
+const element = <h1>Hello,{name}</h1>;
+ReactDom.render(element, document.getElementById("root"));
 reportWebVitals();
+function userName(user) {
+  return `${user.firstname} ${user.lastname}`;
+}
+const user = {
+  firstname: "Dipu",
+  lastname: "Mondal",
+};
+const ele = <h2>Hi!!!,{userName(user)}</h2>;
+ReactDom.render(ele, document.getElementById("root"));
+function tick() {
+  const elemen = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDom.render(elemen, document.getElementById("root"));
+}
+
+setInterval(tick, 1000);
