f98713d2f46950fe97c91aeae4a547586d2fe558
Farhad Ahamed <farhadahamed06@gmail.com>
2021-11-11 12:31:21 +0530

    some change

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from "react";
-import ReactDom from "react-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
-ReactDom.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-reportWebVitals();
+import Reactdom from "react-dom";
+// simple jsx
+const element = <h1>Hello Jsx </h1>;
+Reactdom.render(element, document.getElementById("root"));
+// print dynamic name using jsx
+const name = "Farhad";
+const ele = <h1>Hi ! ! {name}</h1>;
+Reactdom.render(ele, document.getElementById("root"));
+// print dynamic name in jsx using function
+function printName(user) {
+  return `${user.fname} ${user.lname}`;
+}
+const user = {
+  fname: "Rohit",
+  lname: "Sharma",
+};
+const printNam = <h2>{printName(user)}</h2>;
+Reactdom.render(printNam, document.getElementById("root"));
+// example of rendaring
+function Clock() {
+  const element1 = (
+    <div>
+      <h1>India</h1>
+      <h2>IST is : {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+  Reactdom.render(element1,document.getElementById("root"));
+}
+setInterval(Clock, 1000);
