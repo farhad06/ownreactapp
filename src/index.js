2b4ca8f802f8893f18e3ac6f910db375345ecf76
Farhad Ahamed <farhadahamed06@gmail.com>
2021-11-11 13:33:59 +0530

    dd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Reactdom from "react-dom";
+// start example of JSX and Rendaring
 // simple jsx
 const element = <h1>Hello Jsx </h1>;
 Reactdom.render(element, document.getElementById("root"));
@@ -18,13 +19,28 @@ const user = {
 const printNam = <h2>{printName(user)}</h2>;
 Reactdom.render(printNam, document.getElementById("root"));
 // example of rendaring
-function Clock() {
+/* function Clock() {
   const element1 = (
     <div>
       <h1>India</h1>
       <h2>IST is : {new Date().toLocaleTimeString()}</h2>
     </div>
   );
-  Reactdom.render(element1,document.getElementById("root"));
+  Reactdom.render(element1, document.getElementById("root"));
 }
-setInterval(Clock, 1000);
+setInterval(Clock,1000); */
+// end example of JSX and Rendaring
+// start example of component and element
+// function component
+function Timeset({ local }) {
+  return (
+    <div>
+      <h1>India (Kolkata)</h1>
+      <h2>IST is : {new Date().toLocaleTimeString(local)}</h2>
+    </div>
+  );
+}
+Reactdom.render(<Timeset local="bn-BD" />, document.getElementById("root"));
+/* In a function the total function is called react component and return parts is called element */
+// class component
+
